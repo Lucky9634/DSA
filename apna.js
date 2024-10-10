d772401d8a9cca73fd0fb1dddae8a5da3fcada7a
Kamal Hasan Ansari <Kamalhasan12345678@gmail.com>
2024-10-10 19:27:10 +0530

    first commit

---
+++ apna.js
@@ -0,0 +1,6 @@
+// public:
+// pair<long long, long long> getMinMax(vector<long long int> arr) {
+//     // code here
+//     long long maximumElement = INT_MIN;
+//     for(int i = 0; i < arr.size(;))
+// }
